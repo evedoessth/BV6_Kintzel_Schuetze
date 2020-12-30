45cdf576ac29157cb44d35ba0acf94347a9ca8fa
evedoessth <protect#CODE6biz>
2020-12-30 17:52:35 +0100

    test commit over ssh

--- src/bv_ws20_21/Main.java
+++ src/bv_ws20_21/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("ImageAnalysisAppView.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 575));
+        primaryStage.setScene(new Scene(root, 600, 550));
         primaryStage.show();
     }
 
